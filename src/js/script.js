e2d18eb7c06c4d7f4c95aa4d3951a83be659d594
unknown <kamila.wilczynska92@gmail.com>
2022-10-05 21:34:48 +0100

    Cart Active

--- src/js/script.js
+++ src/js/script.js
@@ -327,9 +327,36 @@
       const event = new Event('updated');
       thisWidget.element.dispatchEvent(event);
     }
-
- 
   }
+
+  class Cart {
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+      
+      thisCart.dom ={};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function () {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+
+      });
+    }
+  }
+
 
 
   const app = {
@@ -346,6 +373,12 @@
       const thisApp = this;
       thisApp.data = dataSource;
     },
+    initCart: function(){
+      const thisApp =this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+      console.log('Cart Element',cartElem );
+    },
 
     init: function () {
       const thisApp = this;
@@ -357,7 +390,9 @@
 
       thisApp.initData();
       thisApp.initMenu();
-    },
+      thisApp.initCart();
+
+    }, 
   };
 
 
