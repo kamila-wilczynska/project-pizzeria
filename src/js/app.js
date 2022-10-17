cebcb291cd96545af069de79e35c6fe1f3532057
unknown <kamila.wilczynska92@gmail.com>
2022-10-17 21:53:12 +0100

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 
 
 
-import { settings, select} from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/product.js';
 import Cart from './components/cart.js';
 
@@ -10,6 +10,54 @@ import Cart from './components/cart.js';
 
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /*get  page if href attribute */
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /*run thisApp.activatePage */
+        thisApp.activatePage(id);
+      });
+    }
+
+
+    
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    /*add class "active to matching pages, remove from non-matching" */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(
+        classNames.pages.active,
+        page.id == pageId
+      );
+    }
+    /*add class "active to matching links, remove from non-links" */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+  
+    }
+
+  },
+
+
   initMenu: function () {
     const thisApp = this;
     //console.log('thisApp.data', thisApp.data);
@@ -22,7 +70,8 @@ const app = {
   initData: function () {
     const thisApp = this;
     thisApp.data = {};
-    const url = settings.db.url + '/' + settings.db.products;
+    const url = settings.db.url + '/' + settings.db.product;
+    
     //connect with url
     fetch(url)
     //convert data to JS
@@ -35,7 +84,7 @@ const app = {
 
         /* save parsedResponse at thisApp.data.products */
         thisApp.data.products = parsedResponse;
-        
+
         /* execute initMenu method */
         thisApp.initMenu();
 
@@ -47,7 +96,7 @@ const app = {
 
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
-    
+
     thisApp.productList = document.querySelector(select.containerOf.menu);
     thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
@@ -62,6 +111,8 @@ const app = {
     //console.log('classNames:', classNames);
     //console.log('settings:', settings);
     //console.log('templates:', templates);
+    thisApp.initPages();
+    //thisApp.initMenu();
 
     thisApp.initData();
 
